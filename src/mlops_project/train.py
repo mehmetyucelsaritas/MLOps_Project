f6028d08603a6b749a5b065c06ef73cc109769ef
mehmet <mehmetyucelsaritas19@gmail.com>
2025-12-27 12:45:14 +0100

    configure train.py with wandb

--- src/mlops_project/train.py
+++ src/mlops_project/train.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import torch
 import typer
+import wandb
 
-from mlops_project.data import corrupt_mnist
-from mlops_project.model import MyAwesomeModel
+from data import corrupt_mnist
+from model import MyAwesomeModel
+from sklearn.metrics import RocCurveDisplay, accuracy_score, f1_score, precision_score, recall_score
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
@@ -11,43 +13,55 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.ba
 def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
     """Train a model on MNIST."""
     print("Training day and night")
-    print(f"{lr=}, {batch_size=}, {epochs=}")
+    config = {"lr": lr, "batch_size": batch_size, "epochs": epochs}
+    with wandb.init(project="corrupt_mnist", config=config) as run:
 
-    model = MyAwesomeModel().to(DEVICE)
-    train_set, _ = corrupt_mnist()
+        print(f"{lr=}, {batch_size=}, {epochs=}")
 
-    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
+        model = MyAwesomeModel().to(DEVICE)
+        train_set, _ = corrupt_mnist()
 
-    loss_fn = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
 
-    statistics = {"train_loss": [], "train_accuracy": []}
-    for epoch in range(epochs):
-        model.train()
-        for i, (img, target) in enumerate(train_dataloader):
-            img, target = img.to(DEVICE), target.to(DEVICE)
-            optimizer.zero_grad()
-            y_pred = model(img)
-            loss = loss_fn(y_pred, target)
-            loss.backward()
-            optimizer.step()
-            statistics["train_loss"].append(loss.item())
+        loss_fn = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-            accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
-            statistics["train_accuracy"].append(accuracy)
+        statistics = {"train_loss": [], "train_accuracy": []}
+        for epoch in range(epochs):
+            model.train()
+            for i, (img, target) in enumerate(train_dataloader):
+                img, target = img.to(DEVICE), target.to(DEVICE)
+                optimizer.zero_grad()
+                y_pred = model(img)
+                loss = loss_fn(y_pred, target)
+                loss.backward()
+                optimizer.step()
+                statistics["train_loss"].append(loss.item())
 
-            if i % 100 == 0:
-                print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
+                accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
+                statistics["train_accuracy"].append(accuracy)
+                wandb.log({"train_loss": loss.item(), "train_accuracy": accuracy})
+                if i % 100 == 0:
+                    print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
+                
+
+        torch.save(model.state_dict(), "models/model.pth")
+        artifact = wandb.Artifact(
+        name="corrupt_mnist_model",
+        type="model",
+        description="A model trained to classify corrupt MNIST images",
+        metadata={"accuracy": accuracy, "final_loss": loss.item()},
+        )
+        artifact.add_file("models/model.pth")
+        run.log_artifact(artifact)
 
     print("Training complete")
-    torch.save(model.state_dict(), "models/model.pth")
     fig, axs = plt.subplots(1, 2, figsize=(15, 5))
     axs[0].plot(statistics["train_loss"])
     axs[0].set_title("Train loss")
     axs[1].plot(statistics["train_accuracy"])
     axs[1].set_title("Train accuracy")
     fig.savefig("reports/figures/training_statistics.png")
-
 
 if __name__ == "__main__":
     typer.run(train)
