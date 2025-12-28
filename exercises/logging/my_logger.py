9ee93a9b821accf259263908e07d0e58daccedd7
mehmet <mehmetyucelsaritas19@gmail.com>
2025-12-29 00:49:19 +0100

    include exercises folder

---
+++ exercises/logging/my_logger.py
@@ -0,0 +1,10 @@
+import sys
+from loguru import logger
+
+logger.remove()
+logger.add("./exercises/logging/my_log.log", level="DEBUG", rotation="100 MB")
+logger.debug("Used for debugging your code.")
+logger.info("Informative messages from your code.")
+logger.warning("Everything works but there is something to be aware of.")
+logger.error("There's been a mistake with the process.")
+logger.critical("There is something terribly wrong and process may terminate.")
