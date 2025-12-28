d12cc5490795e66345bf2f2dc3bbcea79559ab43
mehmet <mehmetyucelsaritas19@gmail.com>
2025-12-29 00:41:17 +0100

    trigger cloud build

--- src/mlops_project/model.py
+++ src/mlops_project/model.py
@@ -46,4 +46,4 @@ if __name__ == "__main__":
 
     dummy_input = torch.randn(1, 1, 28, 28)
     output = model(dummy_input)
-    print(f"Output shape: {output.shape}")
+    print(f"Output shape : {output.shape}")
