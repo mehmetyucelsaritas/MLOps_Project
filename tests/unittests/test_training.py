f5dcccd6020067db53cb9f50b2b3545d35ebe050
mehmet <mehmetyucelsaritas19@gmail.com>
2026-01-04 23:30:43 +0300

    Deleted files belonging to exercises

--- tests/unittests/test_training.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-from mlops_project.train import DEVICE, MyAwesomeModel
-
-
-def test_device():
-    device = DEVICE
-    assert device == torch.device("cuda") or torch.device("cpu") or torch.device("mps")
