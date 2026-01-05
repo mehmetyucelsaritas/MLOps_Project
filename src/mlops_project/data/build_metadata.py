b679a83a163bc5bb4bd6eac8108d9e20ddcf7704
mehmet <mehmetyucelsaritas19@gmail.com>
2026-01-05 13:28:57 +0300

    add build_metadata

---
+++ src/mlops_project/data/build_metadata.py
@@ -0,0 +1,25 @@
+import os
+import json
+from mlops_project.model.chunking import Chunking
+
+
+txt_folder = "./data/processed"
+task1_metadata_path = "./data/raw/all_metadata.json"
+output_metadata_path = "./data/processed/task2_metadata.json"
+
+with open(task1_metadata_path, "r", encoding="utf-8") as f:
+    task1_metadata = json.load(f)
+
+task1_table = {entry["filename"]: entry for entry in task1_metadata}
+
+task2_metadata = []
+
+for fname in os.listdir(txt_folder):
+    if fname.endswith(".txt"):
+        txt_path = os.path.join(txt_folder, fname)
+        chunks = Chunking(txt_path, max_words = 300, min_words=100, task1_metadata=task1_table)
+
+        task2_metadata.extend(chunks.chunk_items)
+
+with open(output_metadata_path, "w", encoding="utf-8") as f:
+    json.dump(task2_metadata, f, indent=2, ensure_ascii=False)
